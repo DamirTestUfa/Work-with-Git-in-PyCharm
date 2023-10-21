37e57d529e7318c653509b3368f5cda99d71830e
Ufa1971 <bps100@ya.ru>
2023-10-21 17:20:17 +0500

    добавлен модуль strings.py, удале текст

--- first/nums.py
+++ first/nums.py
@@ -1,11 +1,15 @@
 def plus(a, b):
     return a + b
 
-text = "new text"
 
 def minus(a, b):
     return a-b
 
+def mul(a, b):
+    return a*b
+
+
 if __name__ == '__main__':
     print(plus(2,4))
     print(minus(7,2))
+    print(mul(3, 3))
